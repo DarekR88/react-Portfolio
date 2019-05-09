17e369f4b77c6c8d05f6a0d9ce9276b27d580b34
DarekR88 <dradmclad@gmail.com>
2019-05-09 15:57:31 -0500

    outline

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Hi, My name is Darek</h1>
+        <p className="blurb">I am a full stack website developer.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
