ef62cf9eec91c5423f4f719c6fdd59cfc525cd24
DarekR88 <dradmclad@gmail.com>
2019-05-28 15:45:00 -0500

    pictures added

--- src/components/Blurb/index.js
+++ src/components/Blurb/index.js
@@ -4,6 +4,7 @@ import "./style.css"
 function Blurb() {
     return (
         <div>
+            <img className="portrait" width="250" src="../images/IMG_1237.jpg" alt="portrait"></img>
             <h1>Hello, my name is Darek Radke.</h1>
             <p>I am a full stack website developer.</p>
         </div>
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -4,6 +4,10 @@ import "./style.css"
 function Projects() {
     return (
         <div>
+            <div className="projectDiv">
+                <a className="projectLink" href="https://cafe-del-sol-darek.herokuapp.com/"><img width="250" height="175" src="../images/cafeProject.PNG" alt="scrape"></img></a>
+                <p>Application that allows restaurant owners to maintain and modify their own websites with a convenient and accessible admin portal.</p>
+            </div>
             <div id="first" className="projectDiv">
                 <a className="projectLink" href="https://game-jam2019.herokuapp.com/"><img width="250" height="175" alt="gameJam" src="../images/gameJam.PNG"></img></a>
                 <p>Arcade style website that allows users to play minigames and add their highscore to a database. High scores are displayed on the website.</p>
